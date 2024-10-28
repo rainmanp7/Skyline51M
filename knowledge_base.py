81eac57b24bb3ff0578e398ba45e307c8c5bcba7
Area15 R&D <muslimsoap@gmail.com>
2024-10-28 21:29:56 +0800

    Update knowledge_base.py

--- knowledge_base.py
+++ knowledge_base.py
@@ -1,19 +1,18 @@
-# knowledge_base.py
+
+# Beginning of knowledge_base.py
 from collections import deque
-import asyncio
-import logging
 
 class SimpleKnowledgeBase:
     def __init__(self, max_recent_items=100):
         self.data = {}
         self.recent_updates = deque(maxlen=max_recent_items)
-        self.lock = asyncio.Lock()
 
-    async def update(self, key, value):
-        async with self.lock:
+    def update(self, key, value):
+        with knowledge_lock:
             if key in self.data:
                 self.data[key].extend(value)
                 self.data[key] = list(set(self.data[key]))
             else:
                 self.data[key] = value
             self.recent_updates.append((key, value))
+# End of knowledge_base.py
